41868c643adc59cbded617e04741bf2f44a992b6
pkamanthe <patriciamumbua97@gmail.com>
2025-02-03 14:56:12 +0300

    Initial code

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "./Navbar"; // Import Navbar
+import Navbar from "./Navbar"; 
 import EventList from "./EventList"; 
-import AddEventForm from "./AddEventForm"; // Import the AddEventForm for adding new events
+import AddEventForm from "./AddEventForm"; 
 import "./App.css";
 
 function App() {
   const [events, setEvents] = useState([]);
-  const [searchQuery, setSearchQuery] = useState(""); // Search query state
-  const [view, setView] = useState("all"); // Current view (all, add)
+  const [searchQuery, setSearchQuery] = useState(""); 
 
-  // Fetch initial events from the API
-  useEffect(() => {
+    useEffect(() => {
     fetch("http://127.0.0.1:5000/events")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data); // Check the fetched data
+        console.log(data); 
         setEvents(data);
       })
       .catch((error) => console.error("Error fetching events:", error));
   }, []);
 
-  // Handle adding a new event
+  
   const handleAddEvent = (newEvent) => {
-    // Assuming the newEvent has a structure like: { name, location, category, date }
+   
     setEvents((prevEvents) => [...prevEvents, newEvent]);
-    setView("all"); // Switch back to "all" events view after adding an event
+    setView("all"); 
   };
 
-  // Filter events based on the search query
+
   const filteredEvents = events.filter(
     (event) =>
       event.name.toLowerCase().includes(searchQuery.toLowerCase())
@@ -49,7 +47,7 @@ function App() {
 
       {view === "add" && (
         <AddEventForm
-          handleAddEvent={handleAddEvent} // Pass the function to handle adding new events
+          handleAddEvent={handleAddEvent} 
         />
       )}
     </div>
