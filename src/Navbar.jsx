3e961109afdd9a28e28d8a3ed12c1e7e69430c09
pkamanthe <patriciamumbua97@gmail.com>
2025-02-03 09:46:18 +0300

    Initial code

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,16 +3,21 @@ import React from "react";
 function Navbar({ searchQuery, setSearchQuery, setView }) {
   return (
     <nav className="navbar">
-      {/* Search input */}
+    
+      <button onClick={() => setView("home")}>🏠 Home</button>
+      <button onClick={() => setView("events")}>📅 Events</button>
+
+  
       <input
         type="text"
         placeholder="Search Event..."
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
       />
-      <button onClick={() => setView("all")}>All Events</button>
 
-      {/* Add Event Button */}
+      <button onClick={() => setView("all")}>📌 All Events</button>
+
+      
       <button onClick={() => setView("add")}>➕ Add Event</button>
     </nav>
   );
